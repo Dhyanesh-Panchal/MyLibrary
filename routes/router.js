bc8531726568f3bf918162145ea47ee70bbbcb6c
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-05-07 14:40:15 +0530

    Tries vercel configuration

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("jay shree krishna!  WORking")
+    res.render("index")
 })
 router.get('/hello', (req, res) => {
     res.send("inside route Hello")
